bb8870b1138e203de52a14e6bc61cd3dc1eb6aca
Chanwit Kaewkasi <chanwit@gmail.com>
2015-02-01 22:29:06 +0700

    gofmt

--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"github.com/docker/swarm/common"
 	"github.com/docker/swarm/scheduler/strategy/plugin"
 	"github.com/samalba/dockerclient"
 )
 
 type FirstOnlyStrategy struct{}
 
+func (s *FirstOnlyStrategy) Name() string {
+	return "first"
+}
+
 func (s *FirstOnlyStrategy) Initialize() error {
 	return nil
 }
 
-func (s *FirstOnlyStrategy) PlaceContainer(config *dockerclient.ContainerConfig, nodes []*common.Node) (*common.Node, error) {
+func (s *FirstOnlyStrategy) PlaceContainer(config *dockerclient.ContainerConfig, nodes []*plugin.Node) (*plugin.Node, error) {
 	return nodes[0], nil
 }
 
 func main() {
-	plugin.Run(&FirstOnlyStrategy{})
+	plugin.Run(new(FirstOnlyStrategy))
 }
